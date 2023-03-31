27a766ca12990f25c412c1a608a1b40ce17798c9
jgrie <jgrie@IFYOUFINDMEPLSI>
2023-03-31 10:28:41 +0200

    Add project files.

---
+++ Programming_1A_Assignment_1/Form1.Designer.vb
@@ -0,0 +1,395 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox_name = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox_DtBs = New System.Windows.Forms.TextBox()
+        Me.TextBox_PrgJva = New System.Windows.Forms.TextBox()
+        Me.TextBox_PrgC = New System.Windows.Forms.TextBox()
+        Me.TextBox_DgtLgc = New System.Windows.Forms.TextBox()
+        Me.TextBox_ArtInt = New System.Windows.Forms.TextBox()
+        Me.TextBox_Prg2D = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox_decision = New System.Windows.Forms.TextBox()
+        Me.TextBox_grade = New System.Windows.Forms.TextBox()
+        Me.TextBox_average = New System.Windows.Forms.TextBox()
+        Me.TextBox_total = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button_exit = New System.Windows.Forms.Button()
+        Me.Button_back = New System.Windows.Forms.Button()
+        Me.Button_enter_new_details = New System.Windows.Forms.Button()
+        Me.Button_calculate = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(210, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(342, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "WELCOME TO THE INFORMATION TECHNOLOGY DEPARTMENT"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(222, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter Student Name:"
+        '
+        'TextBox_name
+        '
+        Me.TextBox_name.Location = New System.Drawing.Point(344, 30)
+        Me.TextBox_name.Name = "TextBox_name"
+        Me.TextBox_name.Size = New System.Drawing.Size(191, 23)
+        Me.TextBox_name.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox_DtBs)
+        Me.GroupBox1.Controls.Add(Me.TextBox_PrgJva)
+        Me.GroupBox1.Controls.Add(Me.TextBox_PrgC)
+        Me.GroupBox1.Controls.Add(Me.TextBox_DgtLgc)
+        Me.GroupBox1.Controls.Add(Me.TextBox_ArtInt)
+        Me.GroupBox1.Controls.Add(Me.TextBox_Prg2D)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Yellow
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 51)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(351, 217)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "IT Subjects"
+        '
+        'TextBox_DtBs
+        '
+        Me.TextBox_DtBs.Location = New System.Drawing.Point(165, 184)
+        Me.TextBox_DtBs.Name = "TextBox_DtBs"
+        Me.TextBox_DtBs.Size = New System.Drawing.Size(151, 23)
+        Me.TextBox_DtBs.TabIndex = 9
+        '
+        'TextBox_PrgJva
+        '
+        Me.TextBox_PrgJva.Location = New System.Drawing.Point(165, 152)
+        Me.TextBox_PrgJva.Name = "TextBox_PrgJva"
+        Me.TextBox_PrgJva.Size = New System.Drawing.Size(151, 23)
+        Me.TextBox_PrgJva.TabIndex = 8
+        '
+        'TextBox_PrgC
+        '
+        Me.TextBox_PrgC.Location = New System.Drawing.Point(165, 120)
+        Me.TextBox_PrgC.Name = "TextBox_PrgC"
+        Me.TextBox_PrgC.Size = New System.Drawing.Size(151, 23)
+        Me.TextBox_PrgC.TabIndex = 7
+        '
+        'TextBox_DgtLgc
+        '
+        Me.TextBox_DgtLgc.Location = New System.Drawing.Point(165, 89)
+        Me.TextBox_DgtLgc.Name = "TextBox_DgtLgc"
+        Me.TextBox_DgtLgc.Size = New System.Drawing.Size(151, 23)
+        Me.TextBox_DgtLgc.TabIndex = 6
+        '
+        'TextBox_ArtInt
+        '
+        Me.TextBox_ArtInt.Location = New System.Drawing.Point(165, 56)
+        Me.TextBox_ArtInt.Name = "TextBox_ArtInt"
+        Me.TextBox_ArtInt.Size = New System.Drawing.Size(151, 23)
+        Me.TextBox_ArtInt.TabIndex = 5
+        '
+        'TextBox_Prg2D
+        '
+        Me.TextBox_Prg2D.Location = New System.Drawing.Point(165, 23)
+        Me.TextBox_Prg2D.Name = "TextBox_Prg2D"
+        Me.TextBox_Prg2D.Size = New System.Drawing.Size(151, 23)
+        Me.TextBox_Prg2D.TabIndex = 4
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(18, 187)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(55, 15)
+        Me.Label8.TabIndex = 0
+        Me.Label8.Text = "Database"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(18, 155)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(106, 15)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Programming Java"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(18, 123)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(108, 15)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Programming C++"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(18, 92)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 15)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Digital Logic"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 59)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(115, 15)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Artificial Intelligence"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(98, 15)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Programming 2D"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.TextBox_decision)
+        Me.GroupBox2.Controls.Add(Me.TextBox_grade)
+        Me.GroupBox2.Controls.Add(Me.TextBox_average)
+        Me.GroupBox2.Controls.Add(Me.TextBox_total)
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.ForeColor = System.Drawing.Color.Yellow
+        Me.GroupBox2.Location = New System.Drawing.Point(369, 51)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(357, 217)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Scores"
+        '
+        'TextBox_decision
+        '
+        Me.TextBox_decision.Location = New System.Drawing.Point(31, 179)
+        Me.TextBox_decision.Name = "TextBox_decision"
+        Me.TextBox_decision.Size = New System.Drawing.Size(290, 23)
+        Me.TextBox_decision.TabIndex = 7
+        '
+        'TextBox_grade
+        '
+        Me.TextBox_grade.Location = New System.Drawing.Point(119, 120)
+        Me.TextBox_grade.Name = "TextBox_grade"
+        Me.TextBox_grade.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox_grade.TabIndex = 6
+        '
+        'TextBox_average
+        '
+        Me.TextBox_average.Location = New System.Drawing.Point(119, 79)
+        Me.TextBox_average.Name = "TextBox_average"
+        Me.TextBox_average.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox_average.TabIndex = 5
+        '
+        'TextBox_total
+        '
+        Me.TextBox_total.Location = New System.Drawing.Point(116, 38)
+        Me.TextBox_total.Name = "TextBox_total"
+        Me.TextBox_total.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox_total.TabIndex = 4
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(31, 152)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(52, 15)
+        Me.Label12.TabIndex = 3
+        Me.Label12.Text = "Decision"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(45, 123)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(38, 15)
+        Me.Label11.TabIndex = 2
+        Me.Label11.Text = "Grade"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(45, 82)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(50, 15)
+        Me.Label10.TabIndex = 1
+        Me.Label10.Text = "Average"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(45, 41)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(32, 15)
+        Me.Label9.TabIndex = 0
+        Me.Label9.Text = "Total"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Button_exit)
+        Me.GroupBox3.Controls.Add(Me.Button_back)
+        Me.GroupBox3.Controls.Add(Me.Button_enter_new_details)
+        Me.GroupBox3.Controls.Add(Me.Button_calculate)
+        Me.GroupBox3.ForeColor = System.Drawing.Color.Yellow
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 274)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(714, 83)
+        Me.GroupBox3.TabIndex = 0
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Controls"
+        '
+        'Button_exit
+        '
+        Me.Button_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_exit.Location = New System.Drawing.Point(620, 35)
+        Me.Button_exit.Name = "Button_exit"
+        Me.Button_exit.Size = New System.Drawing.Size(58, 23)
+        Me.Button_exit.TabIndex = 11
+        Me.Button_exit.Text = "Exit"
+        Me.Button_exit.UseVisualStyleBackColor = True
+        '
+        'Button_back
+        '
+        Me.Button_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_back.Location = New System.Drawing.Point(538, 35)
+        Me.Button_back.Name = "Button_back"
+        Me.Button_back.Size = New System.Drawing.Size(59, 23)
+        Me.Button_back.TabIndex = 10
+        Me.Button_back.Text = "Back"
+        Me.Button_back.UseVisualStyleBackColor = True
+        '
+        'Button_enter_new_details
+        '
+        Me.Button_enter_new_details.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_enter_new_details.Location = New System.Drawing.Point(198, 35)
+        Me.Button_enter_new_details.Name = "Button_enter_new_details"
+        Me.Button_enter_new_details.Size = New System.Drawing.Size(151, 23)
+        Me.Button_enter_new_details.TabIndex = 9
+        Me.Button_enter_new_details.Text = "Enter New Details"
+        Me.Button_enter_new_details.UseVisualStyleBackColor = True
+        '
+        'Button_calculate
+        '
+        Me.Button_calculate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button_calculate.Location = New System.Drawing.Point(39, 35)
+        Me.Button_calculate.Name = "Button_calculate"
+        Me.Button_calculate.Size = New System.Drawing.Size(113, 23)
+        Me.Button_calculate.TabIndex = 8
+        Me.Button_calculate.Text = "Calculate"
+        Me.Button_calculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(739, 368)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox_name)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox_name As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox_DtBs As TextBox
+    Friend WithEvents TextBox_PrgJva As TextBox
+    Friend WithEvents TextBox_PrgC As TextBox
+    Friend WithEvents TextBox_DgtLgc As TextBox
+    Friend WithEvents TextBox_ArtInt As TextBox
+    Friend WithEvents TextBox_Prg2D As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents TextBox_decision As TextBox
+    Friend WithEvents TextBox_grade As TextBox
+    Friend WithEvents TextBox_average As TextBox
+    Friend WithEvents TextBox_total As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button_exit As Button
+    Friend WithEvents Button_back As Button
+    Friend WithEvents Button_enter_new_details As Button
+    Friend WithEvents Button_calculate As Button
+End Class
---
+++ Programming_1A_Assignment_1/Form1.vb
@@ -0,0 +1,187 @@
+﻿Public Class Form1
+
+    'Declaring variables.
+
+    '>>Inputs
+    Public mark_Prg2D As String
+    Public mark_ArtInt As String
+    Public mark_DgtLgc As String
+    Public mark_PrgC As String
+    Public mark_PrgJva As String
+    Public mark_DtBs As String
+
+    '>>Outputs
+    Public mark_total As Integer
+    Public average As Integer
+    Public grade As String
+    Public decision As String
+
+    '>>Other
+    Public application_is_active As Boolean = True
+    Public marks_string_array(5) As String
+    Public marks_integer_array(5) As Integer
+    Public boolean_grade_array(5) As Integer
+    Public Decision_val As Integer
+
+    'Display output when clicked
+    Private Sub Button_calculate_Click(sender As Object, e As EventArgs) Handles Button_calculate.Click
+        ClearOutput()
+
+        If (isCorrectFormat(TextBox_Prg2D.Text)) Then
+            mark_Prg2D = TextBox_Prg2D.Text
+        End If
+
+        If (isCorrectFormat(TextBox_ArtInt.Text)) Then
+        End If
+
+        mark_ArtInt = TextBox_ArtInt.Text
+        If (isCorrectFormat(TextBox_DgtLgc.Text)) Then
+            mark_DgtLgc = TextBox_DgtLgc.Text
+        End If
+
+        If (isCorrectFormat(TextBox_PrgC.Text)) Then
+            mark_PrgC = TextBox_PrgC.Text
+        End If
+
+        If (isCorrectFormat(TextBox_PrgJva.Text)) Then
+            mark_PrgJva = TextBox_PrgJva.Text
+        End If
+
+        If (isCorrectFormat(TextBox_DtBs.Text)) Then
+            mark_DtBs = TextBox_DtBs.Text
+        End If
+
+
+        marks_integer_array = {mark_Prg2D, mark_ArtInt, mark_DgtLgc, mark_PrgC, mark_PrgJva, mark_DtBs}
+        'Print Total.
+        TextBox_total.Text = myTotal(marks_integer_array)
+
+        'Calculating average.       xbar = sum of x / n
+        average = myTotal(marks_integer_array) / 6
+
+        'Print Average.
+        TextBox_average.Text = average
+
+        'Print Grade.
+        TextBox_grade.Text = myGrade(average)
+
+        'Print Decision.
+        TextBox_decision.Text = isPass(marks_integer_array)
+
+    End Sub
+
+    'Close forms when clicked.
+    Private Sub Button_exit_Click(sender As Object, e As EventArgs) Handles Button_exit.Click
+        ApplicationExit()
+    End Sub
+
+    Public Sub ApplicationExit()
+        Application.Exit()
+    End Sub
+
+    'Clear all entries when clicked
+    Private Sub Button_enter_new_details_Click(sender As Object, e As EventArgs) Handles Button_enter_new_details.Click
+        ClearInput()
+        ClearOutput()
+    End Sub
+
+    Public Sub ClearInput()
+        'Clearing all TextBox inputs.
+        TextBox_name.Clear()
+
+        TextBox_Prg2D.Clear()
+        TextBox_ArtInt.Clear()
+        TextBox_DgtLgc.Clear()
+        TextBox_PrgC.Clear()
+        TextBox_PrgJva.Clear()
+        TextBox_DtBs.Clear()
+    End Sub
+
+    Public Sub ClearOutput()
+        'Clearing all TextBox outputs.
+        TextBox_total.Clear()
+        TextBox_average.Clear()
+        TextBox_grade.Clear()
+        TextBox_decision.Clear()
+
+        'Clearing all arrays.
+        ReDim marks_integer_array(5)
+        ReDim boolean_grade_array(5)
+    End Sub
+
+    'Exception handling. Check if input is in the correct format (integer), and within range (0-100).
+    Private Function isCorrectFormat(mark As String) As Boolean
+        Dim temp As Integer
+
+        If (String.IsNullOrEmpty(mark)) Then
+            MessageBox.Show("Empty field: Please confirm your input")
+            Return False
+        ElseIf (Not Integer.TryParse(mark, temp)) Then
+            MessageBox.Show("Incorrect Format: Please confirm your input")
+            Return False
+        ElseIf mark < 0 Or mark > 100 Then
+            MessageBox.Show(("Input out of range. Please check your input."))
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Function myTotal(marks_integer_array As Integer()) As Integer
+        mark_total = 0
+
+        For i As Integer = 1 To 6
+            'Summing all values in the marks_integer_array().
+            mark_total += marks_integer_array(i - 1)
+        Next
+
+        Return mark_total
+    End Function
+
+    Private Function myGrade(average As Integer) As String
+        'Determining Grade by comparing the Average.
+        Select Case average
+            Case 0 To 49
+                grade = "Fail"
+            Case 50 To 59
+                grade = "Pass"
+            Case 60 To 69
+                grade = "Credit"
+            Case 70 To 74
+                grade = "Merit"
+            Case 75 To 100
+                grade = "Distinction"
+            Case Else
+                grade = "Mark out of range"
+        End Select
+        Return grade
+    End Function
+
+    Public Function isPass(marks_integer_array As Integer()) As String
+        Decision_val = 0
+        For i As Integer = 1 To 6
+
+            'Pass = 0, Fail = 1
+            If marks_integer_array(i - 1) >= 0 And marks_integer_array(i - 1) <= 49 Then
+                Decision_val += 1
+            End If
+
+            'Determine Decision based on the total value of Decision_val.
+            Select Case Decision_val
+                Case 0
+                    decision = "Proceed"
+                Case 1
+                    decision = "Proceed, carrying 1 Failed subject"
+                Case 2
+                    decision = "Proceed, carrying 2 Failed subject"
+                Case 3
+                    decision = "Proceed, carrying 3 Failed subject"
+                Case 4
+                    decision = "Repeat Semester"
+                Case Else
+                    decision = "N/A"
+            End Select
+        Next
+        Return decision
+    End Function
+End Class
